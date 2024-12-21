b297c0312391564296be42ea9778f14a8c8d310d
Pankaj <pankajchouhan95@hotmail.com>
2024-12-21 10:36:43 +0530

    Add Test case

---
+++ test/product_bloc_test.dart
@@ -0,0 +1,35 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:http/http.dart' as http;
+import 'package:remote_task/appController.dart';
+import 'package:remote_task/models/product.dart';
+import 'dart:convert';
+
+import 'package:remote_task/product/product_bloc.dart';
+import 'package:remote_task/product/product_event.dart';
+import 'package:remote_task/product/product_state.dart';
+import 'package:remote_task/services/apiService.dart';
+
+
+
+void main() {
+  test('Fetch products from API', () async {
+    final apiService = ApiService();
+    final products = await apiService.fetchProducts();
+
+    expect(products, isNotEmpty);
+    expect(products[0].title, isNotNull);
+    expect(products[0].coordinates, isNotNull);
+  });
+
+  group('Distance Calculation', () {
+    test('calculates distance correctly', () {
+      // Assuming you have a function to calculate distance
+      final distance = AppController.calculateDistance(37.7749, -122.4194, 34.0522, -118.2437);
+
+      // Assert
+      expect(distance, isA<String>()); // Assuming it returns a String
+      expect(distance, contains('km')); // Check if it contains the unit
+    });
+  });
+}
